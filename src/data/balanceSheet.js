3eb21a704efee86c694f5d9d7faccced9b1ae7ae
John Grimes <john@grimes.id.au>
2017-05-08 21:34:01 +1000

    Going back to tail recursion method, performs better
    This reverts commit 0309127f79a678c07d979cc20a12d6fc4488b788.

--- src/data/balanceSheet.js
+++ src/data/balanceSheet.js
@@ -20,31 +20,26 @@ export const balanceSheetOverTime = (assets: AssetMap, liabilities: LiabilityMap
                                      startDate: string, endDate: string): BalanceSheetOverTime => {
   const startDay = moment(startDate, DateFormat).startOf('day')
   const endDay = moment(endDate, DateFormat).startOf('day')
-  const days = getDaysWithinRange(startDay, endDay)
-  return days.reduce((acc, day) => {
-    const totalAssets = sumAssetValueAtDate(_.values(assets), day)
-    const totalLiabilities = 0  // sumLiabilityValueAtDate(liabilities, day)
-    return { ...acc,
-      [day.format(DateFormat)]: {
-        totalAssets,
-        totalLiabilities,
-        equity: (totalAssets - totalLiabilities)
-      }
+  return calcBalanceSheet(assets, liabilities, startDay, endDay)
+}
+
+const calcBalanceSheet = (assets: AssetMap, liabilities: LiabilityMap,
+                          date: Moment, endDate: Moment): BalanceSheetOverTime => {
+  if (date.isAfter(endDate)) return {}
+  const totalAssets = sumAssetValueAtDate(_.values(assets), date)
+  const totalLiabilities = 0  // sumLiabilityValueAtDate(liabilities, date)
+  const nextDate = moment(date.add(1, 'days'))
+  return { ...calcBalanceSheet(assets, liabilities, nextDate, endDate),
+    [date.format(DateFormat)]: {
+      totalAssets,
+      totalLiabilities,
+      equity: (totalAssets - totalLiabilities)
     }
-  }, {})
+  }
 }
 
 const sumAssetValueAtDate = (assets: Asset[], date: string): number => {
   if (assets.length === 0) return 0
   const { head, tail } = { head: _.head(assets), tail: _.tail(assets) }
   return getValuationAtDate(head, date) + sumAssetValueAtDate(tail, date)
-}
-
-const getDaysWithinRange = (startDay: Moment, endDay: Moment): Moment[] => {
-  const currentDay = moment(startDay)
-  const moments = []
-  while (!currentDay.isAfter(endDay)) {
-    moments.push(moment(currentDay.add(1, 'days')))
-  }
-  return moments
 }
