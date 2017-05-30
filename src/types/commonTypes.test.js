8c82c4baa4333107e6875d84d2d37f45ae59a65c
John Grimes <john@grimes.id.au>
2017-05-30 16:47:49 +1000

    Add a few more cases to formatNumber test

--- src/types/commonTypes.test.js
+++ src/types/commonTypes.test.js
@@ -5,6 +5,8 @@ import { formatNumber, formatDollars } from 'types/commonTypes'
 describe('formatNumber', () => {
   it('should format a number correctly', () => {
     expect(formatNumber(3157852)).toBe('3,157,852')
+    expect(formatNumber(852)).toBe('852')
+    expect(formatNumber(1000)).toBe('1,000')
   })
 })
 
