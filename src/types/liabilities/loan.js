5a1cb6603d83bb84063540a06d38269358af091e
John Grimes <john@grimes.id.au>
2017-07-08 20:59:40 +1000

    Add simple loan fields to loan type

--- src/types/liabilities/loan.js
+++ src/types/liabilities/loan.js
@@ -5,7 +5,12 @@ import type { Liability } from 'types/liabilities/liability'
 export type Loan = {
   +name: string,
   +startDate: string,
-  +endDate: string
+  +endDate: string,
+  +principal: number,
+  +compoundingPeriod: 'daily'|'monthly',
+  +lengthInYears: number,
+  +establishmentFees: number,
+  +repaymentType: 'principalAndInterest'|'interestOnly',
 }
 
 export type LoanWithId = Loan & { +id: string }
@@ -16,6 +21,11 @@ export const LoanDefaults = {
   name: '',
   startDate: '',
   endDate: '',
+  principal: 0,
+  compoundingPeriod: 'daily',
+  lengthInYears: 25,
+  establishmentFees: 0,
+  repaymentType: 'principalAndInterest',
 }
 
 export const loanToLiability = (loan: Loan): Liability => {
