756f05a6af7a0f1ecefd532fc5953b9b45bc40a3
John Grimes <john@grimes.id.au>
2017-07-21 14:51:35 +1000

    Make chart 100% width, and style tooltip

--- src/components/BalanceSheetChart.js
+++ src/components/BalanceSheetChart.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts'
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts'
 import _ from 'lodash'
 import moment from 'moment'
 
@@ -23,46 +31,36 @@ class BalanceSheetChart extends React.Component {
   processedData() {
     const data = this.props.balanceSheet
     const dates = _.keys(data)
-    const interval = Math.ceil(dates.length / 100) // TODO: Convert 100 into a prop
+    const interval = Math.ceil(dates.length / 200) // TODO: Convert 100 into a prop
     const filteredDates = _.filter(dates, (_, i) => i % interval === 0)
     const filteredData = _.pick(data, filteredDates)
     return _.map(filteredData, (v, k) => ({
-      ...v,
-      date: moment(k, DateStorageFormat).format(DateDisplayFormat),
+      Assets: v.totalAssets,
+      Liabilities: v.totalLiabilities,
+      Equity: v.equity,
+      Date: moment(k, DateStorageFormat).format(DateDisplayFormat),
     }))
   }
 
   render() {
     const data = this.processedData()
     return (
-      <AreaChart data={data} width={900} height={400}>
-        <Area type='monotone' dataKey='totalAssets' />
-        <XAxis dataKey='date' />
-        <YAxis
-          dataKey='totalAssets'
-          tickFormatter={tick => formatDollars(tick)}
-        />
-        <CartesianGrid strokeDasharray='3 3' />
-        <Tooltip
-          formatter={x => {
-            console.log(x)
-            return x
-          }}
-        />
-      </AreaChart>
+      <div className='balance-sheet-chart'>
+        <ResponsiveContainer width='100%' height={200}>
+          <AreaChart data={data}>
+            <Area type='monotone' dataKey='Assets' />
+            <XAxis dataKey='Date' />
+            <YAxis
+              dataKey='Assets'
+              tickFormatter={tick => formatDollars(tick)}
+            />
+            <CartesianGrid strokeDasharray='3 3' />
+            <Tooltip formatter={x => formatDollars(x)} />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     )
   }
 }
-
-// class DollarLabel extends React.Component {
-//   render() {
-//     const { x, y, payload } = this.props
-//     return (
-//       <text x={x} y={y}>
-//         {formatDollars(payload.value)}
-//       </text>
-//     )
-//   }
-// }
 
 export default BalanceSheetChart
